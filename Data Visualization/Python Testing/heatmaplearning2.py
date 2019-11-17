1b19f81dfae34852bb28766bcb9c9ebeeb31c2d5
Morgan Williams <willim15@rpi.edu>
2019-11-17 18:07:07 -0500

    TBC More Heat Map

--- Data Visualization/Python Testing/heatmaplearning2.py
+++ Data Visualization/Python Testing/heatmaplearning2.py
@@ -12,3 +12,19 @@ pd.Series(nums).hist(bins=30)
 # <matplotlib.axes._subplots.AxesSubplot object at 0x10d340d90>
 # returns object in memory
 plt.show()
+
+
+'''
+It turns out that generating a heat map of all the feature variables — feature variables as row headers and column headers, 
+and the variable vs itself on the diagonal— is extremely powerful way to visualize relationships between variables in high dimensional space.
+For example, a correlation matrix with heat map coloring. 
+
+A covariance matrix with heat map coloring. Even a massive confusion matrix with coloring.
+Think less about the traditional use of heat map, but more like color is another dimension that can visually summarize the underlining data.
+
+https://medium.com/data-science-bootcamp/data-visualization-in-machine-learning-beyond-the-basics-baf2cbea8989
+'''
+
+
+#TODO: 
+#https://blog.exploratory.io/quick-introduction-to-heatmap-c21a9f9e4644
